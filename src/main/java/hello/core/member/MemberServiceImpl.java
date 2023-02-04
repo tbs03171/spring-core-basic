eab0aee686af4b03905d6f553cd1936ebc01e116
ParkHyunJung <tbs03171@naver.com>
2023-02-04 21:27:32 +0900

    20230204

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
